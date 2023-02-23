7ee110086312c5bb04fe2ebd3a3c73bb23f6391d
Nirmal <nirmalraj1025@gmail.com>
2023-02-24 00:40:29 +0530

    dropdwon

--- client/src/Components/Dropdown/Dropdown.js
+++ client/src/Components/Dropdown/Dropdown.js
@@ -14,12 +14,16 @@ function Dropdown() {
     <div>
       <select onChange={(e) => setIndex(e.target.value)}>
         {countries.map((item, i) => (
-          <option value={i}>{item.name}</option>
+          <option key={i} value={i}>
+            {item.name}
+          </option>
         ))}
       </select>
       <select>
-        {countries[index].cities.map((val) => (
-          <option value={val}>{val}</option>
+        {countries[index].cities.map((val, i) => (
+          <option key={i} value={val}>
+            {val}
+          </option>
         ))}
       </select>
     </div>
