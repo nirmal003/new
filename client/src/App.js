3517eb1b403613c327ce822096b87fb216f59558
Nirmal <nirmalraj1025@gmail.com>
2024-01-27 17:58:30 +0530

    Send FormData with file

--- client/src/App.js
+++ client/src/App.js
@@ -14,20 +14,30 @@ function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [avatar, setImage] = useState(null);
+  const [file, setImage] = useState(null);
+  const [data, setData] = useState(null);
+
   // const [light, setLight] = useState(false);
   // const [show, setShow] = useState(false);
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
-    console.log(name, email, password, avatar);
+    console.log(name, email, password, file);
 
-    await axios.post("http://localhost:4000/api/v1/register", {
-      name,
-      email,
-      password,
-      avatar,
-    });
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("file", file);
+
+    const result = await axios.post(
+      "http://localhost:4000/api/v1/register",
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+
+    console.log(result.data);
+    setData(result.data);
 
     // if (!name || !password) {
     //   document.getElementById("nameErr").innerText = "Please fill the userName";
@@ -40,15 +50,15 @@ function App() {
   //   setLight(!light);
   // };
 
-  const haldleOnChange = async (e) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setImage(reader.result);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  };
+  // const haldleOnChange = async (e) => {
+  //   const reader = new FileReader();
+  //   reader.onload = () => {
+  //     if (reader.readyState === 2) {
+  //       setImage(reader.result);
+  //     }
+  //   };
+  //   reader.readAsDataURL(e.target.files[0]);
+  // };
 
   return (
     <>
@@ -83,7 +93,7 @@ function App() {
             type="file"
             placeholder="image"
             accept="image/*"
-            onChange={haldleOnChange}
+            onChange={(e) => setImage(e.target.files[0])}
           />
           {/* <span id="passErr"></span> */}
           <input type="submit" value="Register" />
@@ -105,6 +115,12 @@ function App() {
       {/* <a href="mailto:nirmalraj1025@gmail.com">email</a> */}
 
       {/* <Demo /> */}
+
+      {data && (
+        <>
+          <img src={`${data.user.avatar.url}`} alt="loading" />
+        </>
+      )}
     </>
   );
 }
